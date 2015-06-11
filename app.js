38512228d1c58a639417ff3e3a3e292f0f85b865
DanielFGray <DanielFGray@gmail.com>
2015-06-11 18:41:37 -0500

    resolves #2

--- app.js
+++ app.js
@@ -15,7 +15,6 @@ var table = new blessed.table({
 	height:          13,
 	noCellBorders:   true,
 	tags:            true,
-	label:           mycal.getTitle(),
 	border:          'line',
 	style: {
 		border: {
@@ -24,16 +23,37 @@ var table = new blessed.table({
 	},
 });
 
-table.setData(mycal.getArray());
-
 screen.append(table);
 
-screen.key(['Up'], function(ch, key) {
-	
+function doTable(cal) {
+	table.setData(cal.getArray());
+	table.setLabel(cal.getTitle());
+	screen.append(table);
+	screen.render();
+}
+
+screen.key(['down','left','j','h'], function(ch, key) {
+	var prevmonth = mycal.month -1, year = mycal.year;
+	if (prevmonth < 0 ) {
+		prevmonth = 11; 
+		year--;
+	}
+	mycal = new cal(prevmonth, year);
+	doTable(mycal);
+});
+
+screen.key(['up','right','k','l'], function(ch, key) {
+	var nextmonth = mycal.month +1, year = mycal.year;
+	if (nextmonth > 11 ) {
+		nextmonth = 0; 
+		year++;
+	}
+	mycal = new cal(nextmonth, year);
+	doTable(mycal);
 });
 
 screen.key(['escape', 'q', 'C-c'], function(ch, key) {
 	return process.exit(0);
 });
 
-screen.render();
+doTable(mycal);
