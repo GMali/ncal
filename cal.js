28a83904e2d21b9181051bacbe4d6c848fe8604e
Gaurav Mali <hello@gauravmali.com>
2015-08-12 21:00:15 -0700

    Refactoring cal.js
     - Moving some of the logic in getArray to their own methods
     - Changed the spacing from tabs to 4 spaces (Text Editor setting)
     - Using === instead of ==. Just a good practice. Less error prone.
     - Removed unused constants
     - Renamed constants in CAPS_SNAKE_CASE

--- cal.js
+++ cal.js
@@ -1,44 +1,70 @@
-var daysLabels = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-var monthsLabels = ['January', 'February', 'March', 'April',
-                     'May', 'June', 'July', 'August', 'September',
-                     'October', 'November', 'December'];
-var daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-
-var currentDate = new Date(); 
+var DAYS = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+var MONTHS = [
+    { days : 31, label : 'January'   },
+    { days : 28, label : 'February'  },
+    { days : 31, label : 'March'     },
+    { days : 30, label : 'April'     },
+    { days : 31, label : 'May'       },
+    { days : 30, label : 'June'      },
+    { days : 31, label : 'July'      },
+    { days : 31, label : 'August'    },
+    { days : 30, label : 'September' },
+    { days : 31, label : 'October'   },
+    { days : 30, label : 'November'  },
+    { days : 31, label : 'December'  },
+];
 
 function Calendar(month, year) {
-	this.month  = (isNaN(month) || month == null) ? currentDate.getMonth()    : month;
-	this.year   = (isNaN(year)  || year == null)  ? currentDate.getFullYear() : year;
+    var now = new Date();
+
+    // Don't use `month || ...` here because month can be `0`.
+    this.month = month === undefined ? now.getMonth() : month;
+    this.year = year || now.getFullYear();
+
+    // Days in February
+    if (this.isLeapYear()) MONTHS[1].days = 29;
 }
 
 Calendar.prototype.getTitle = function() {
-	return  ' ' + monthsLabels[this.month] + ' ' + this.year + ' ';
+    return ' ' + MONTHS[this.month].label + ' ' + this.year + ' ';
 };
 
 Calendar.prototype.getArray = function() {
-	var firstDay = new Date(this.year, this.month, 1);
-	var startingDay = firstDay.getDay();
-	var monthLength = daysInMonth[this.month];
-	if (this.month == 1 && ((this.year % 4 == 0 && this.year % 100 != 0) || this.year % 400 == 0)) {
-		monthLength = 29;
-	}
-	var arr = [['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']];
-	for (var i = 0, day = 1; day <= monthLength; i++) {
-		var week = [];
-		for (var j = 0; j <= 6; j++) { 
-			var label = '  ';
-			if (day <= monthLength && (i > 0 || j >= startingDay)) {
-				label = day;
-				if(day == currentDate.getDate() && this.month == currentDate.getMonth())
-					label = '{underline}'+label+'{/underline}'
-				label = (day < 10 ? ' '+label : label);
-				day++;
-			}
-			week.push(label + ' ');
-		}
-		arr.push(week);
-	}
-	return arr;
+    var firstDay = new Date(this.year, this.month, 1);
+    var startingDay = firstDay.getDay();
+    var monthLength = MONTHS[this.month].days;
+
+    var arr = [['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']];
+
+    for (var i = 0, day = 1; day <= monthLength; i++) {
+
+        var week = DAYS.map(function(value, index) {
+            var label = '  ';
+            if (day <= monthLength && (i > 0 || index >= startingDay)) {
+                label = this.formatDay(day);
+                day++;
+            }
+            return label;
+        }.bind(this));
+
+        arr.push(week);
+    }
+
+    return arr;
 }
+
+Calendar.prototype.isLeapYear = function() {
+    return (this.year % 4 === 0 && this.year % 100 !== 0) || this.year % 400 === 0;
+};
+
+Calendar.prototype.isToday = function(day) {
+    var now = new Date();
+    return day === now.getDate() && this.month === now.getMonth();
+};
+
+Calendar.prototype.formatDay = function(day) {
+    var label = this.isToday(day) ? '{underline}' + day + '{/underline}' : day.toString();
+    return day < 10 ? ' ' + label : label;
+};
 
 module.exports = Calendar;
